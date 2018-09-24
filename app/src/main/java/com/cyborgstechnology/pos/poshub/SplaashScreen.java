5b655975ee4d998325b29c6c5b29909184b361e2
zaid894 <zaid.haimoor@yahoo.com>
2018-09-24 14:28:10 +0300

    Initial commit

---
+++ app/src/main/java/com/cyborgstechnology/pos/poshub/SplaashScreen.java
@@ -0,0 +1,13 @@
+package com.cyborgstechnology.pos.poshub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplaashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splaash_screen);
+    }
+}
